a5954e442dfb28eb2d7e6a97c217a4b4ddacfcc7
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-05-30 16:58:58 +0200

    Send mode error to Sentry

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -9,15 +9,24 @@
 
 <script lang="ts">
 	import { page } from '$app/stores';
+	import * as Sentry from '@sentry/browser';
 
 	import type { Load } from '@sveltejs/kit';
 
 	export let status = $page.status;
+	export let error = $page.error;
+
+	Sentry.captureException(error, (scope) => {
+		scope.setExtra('error', error);
+		scope.setTag('error_type', 'error_page');
+		return scope;
+	});
 </script>
 
 <main>
 	<h1>{$t('error.shit.happens')} ({status})</h1>
 	<p>{$t(`error.${status}`, { default: $t('error.default') })}</p>
+	<p>{error}</p>
 	<br />
 	<br />
 
