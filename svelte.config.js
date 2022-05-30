eac075c19b7883339a1e67c656c20598d6d60c7e
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-05-30 13:48:56 +0200

    Remove postcss and fix configs

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess({
-		postcss: true,
+		postcss: false,
 		scss: {
 			sourceMap: true,
 			includePaths: [path.resolve(__dirname, './src/styles')]
@@ -28,7 +28,19 @@ const config = {
 			// instead of creating a single one for the entire app.
 			// if `edge` is true, this option cannot be used
 			// split: false
-		})
+		}),
+		vite: {
+			resolve: {
+				alias: {
+					$styles: path.resolve(__dirname, './src/styles'),
+					$translations: path.resolve(__dirname, './src/translations'),
+					$models: path.resolve(__dirname, './src/models'),
+					$stores: path.resolve(__dirname, './src/stores'),
+					$graphql: path.resolve(__dirname, './src/graphql'),
+					$icons: path.resolve(__dirname, './src/icons')
+				}
+			}
+		}
 	}
 };
 
