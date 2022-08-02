a5e066ec6af651a62ab49fbed30ad04d5655ad12
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-08-02 08:53:52 +0200

    Add header component to all pages

---
+++ src/lib/commons/Header.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { dashedName } from '$stores/profile';
+	import Nav from '$lib/commons/Nav.svelte';
+</script>
+
+<header class="header">
+	<p class="header__name">{$dashedName}</p>
+
+	<Nav />
+</header>
+
+<style lang="scss">
+	@use 'lib/breakpoints' as br;
+
+	.header {
+		position: relative;
+		background-color: var(--primary-2);
+		border-radius: var(--border-radius) var(--border-radius) 0 0;
+		border-right: 1px solid var(--lines);
+		border-left: 1px solid var(--lines);
+		border-top: 1px solid var(--lines);
+		padding: 1.0625rem;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		height: var(--mobile-nav-height);
+		color: var(--secondary-1);
+
+		&__name {
+			font-weight: 400;
+			font-size: 1rem;
+			line-height: 1.3125;
+		}
+
+		@include br.desktop {
+			padding: 1.0625rem 0 1.0625rem 1.375rem;
+			justify-content: flex-start;
+			gap: 9.625rem;
+		}
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,7 +4,6 @@
 
 	import dayjs from 'dayjs';
 	import localeData from 'dayjs/plugin/localeData.js';
-	import { LangEnum } from '$models/langs.enum';
 	import { loadTranslations, t } from '$translations';
 	import { get } from 'svelte/store';
 	import { client } from '$stores/graphql';
@@ -47,64 +46,28 @@
 	import 'sanitize.css/reduce-motion.css';
 
 	import { page } from '$app/stores';
-	import { getRoute, RoutesEnum } from '$lib/routing';
-	import { dashedName } from '$stores/profile';
-	import Icon from '$lib/SvgIcon.svelte';
-	import Burger from '$icons/burger.svg?raw';
-	import Cross from '$icons/cross.svg?raw';
+	import { picture } from '$stores/profile';
 
-	let open = false;
+	import Header from '$lib/commons/Header.svelte';
 </script>
 
 <svelte:head>
-	<title>{$t('common.page.title')}</title>
-	<meta name="description" content={$t('collaborations.pageDescription')} />
 	<meta name="author" content="Mickaël Depardon" />
 	<meta name="robots" content="index,follow" />
 	<meta property="og:url" content="{$page.url.origin}{$page.url.pathname}" />
 	<meta property="og:site_name" content="mickael-depardon.fr" />
 	<meta property="og:type" content="website" />
-	<meta property="og:title" content={$t('common.page.title')} />
-	<meta property="og:description" content={$t('collaborations.pageDescription')} />
-	<!-- <meta
-		property="og:image"
-		content="https://storage.googleapis.com/wizbii-images/New%20Homepage/Collaborations/og-image.png"
-	/> -->
+	<link rel="canonical" href="{$page.url.origin}{$page.url.pathname}" />
+
+	<meta property="og:image" content={$picture} />
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:site" content="@squelix" />
 	<meta name="twitter:creator" content="@squelix" />
 	<meta property="twitter:url" content="{$page.url.origin}{$page.url.pathname}" />
-	<meta name="twitter:title" content={$t('common.page.title')} />
-	<meta name="twitter:description" content={$t('collaborations.pageDescription')} />
-	<!-- <meta
-		name="twitter:image"
-		content="https://storage.googleapis.com/wizbii-images/New%20Homepage/Collaborations/og-image.png"
-	/> -->
-
-	<link rel="alternate" hreflang="fr" href="{$page.url.origin}{$page.url.pathname}" />
-	<link
-		rel="alternate"
-		hreflang="en"
-		href="{$page.url.origin}{getRoute('/en', LangEnum.en_GB, RoutesEnum.Home)}"
-	/>
-	<link rel="canonical" href="{$page.url.origin}{$page.url.pathname}" />
+	<meta name="twitter:image" content={$picture} />
 </svelte:head>
 
-<header class="header">
-	<p class="header__name">{$dashedName}</p>
-
-	<nav class="nav">
-		<button class="btn-clean nav__btn" aria-expanded={open} on:click={() => (open = !open)}>
-			<Icon data={open ? Cross : Burger} />
-		</button>
-
-		<ul class="nav__links" class:nav__links--visible={open}>
-			<li>
-				<a href="/">Home</a>
-			</li>
-		</ul>
-	</nav>
-</header>
+<Header />
 
 <main class="main">
 	<slot />
@@ -115,57 +78,12 @@
 		@import 'app.scss';
 	}
 
-	.header {
-		position: relative;
-		background-color: var(--primary-2);
-		box-shadow: 0 4px 4px rgb(0 0 0 / 25%);
-		border-radius: var(--border-radius) var(--border-radius) 0 0;
-		border-right: 1px solid var(--lines);
-		border-left: 1px solid var(--lines);
-		border-top: 1px solid var(--lines);
-		padding: 1.0625rem;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		height: var(--mobile-nav-height);
-
-		&__name {
-			font-weight: 450;
-			font-size: 1rem;
-			line-height: 1.3125;
-		}
-	}
-
-	.nav {
-		&__links {
-			position: absolute;
-			margin: 0;
-			overflow: hidden;
-			max-height: 0;
-			height: 100vh;
-			top: 55px;
-			left: -1px;
-			right: -1px;
-			opacity: 0;
-			background-color: var(--primary-2);
-			border-radius: 0 0 var(--border-radius) var(--border-radius);
-			border: 1px solid var(--lines);
-			box-shadow: 0 4px 4px rgb(0 0 0 / 25%);
-			transition: max-height 150ms ease-in-out, opacity 150ms ease-in-out, z-index 150ms ease-in-out;
-
-			&--visible {
-				z-index: 1;
-				opacity: 1;
-				max-height: calc(100vh - (var(--mobile-main-padding) * 2) - var(--mobile-nav-height));
-			}
-		}
-	}
-
 	.main {
 		background: var(--primary-2) url('/svg/bg-blur.svg') no-repeat;
 		background-size: cover;
 		border: 1px solid var(--lines);
 		box-shadow: 0 4px 4px rgb(0 0 0 / 25%);
 		border-radius: 0 0 var(--border-radius) var(--border-radius);
+		height: calc(100vh - (var(--mobile-main-padding) * 2) - var(--mobile-nav-height));
 	}
 </style>
