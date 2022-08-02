8bf63aa737ea52fba427961cbb6f7614c8e408e3
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-08-02 08:52:55 +0200

    Update Svelte

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const get: RequestHandler = async ({ request }) => {
+export const GET: RequestHandler = async ({ request }) => {
 	const acceptLanguage = request.headers.get('accept-language');
 	const locale = `${acceptLanguage?.match(/[a-zA-Z]+?(?=-|_|,|;)/)}`.toLowerCase();
 
