ead136059ad177780505e148aa95397ac8f90f86
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-08-06 09:56:56 +0200

    Refactor base redirect

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,10 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async ({ request }) => {
-	const acceptLanguage = request.headers.get('accept-language');
-	const locale = `${acceptLanguage?.match(/[a-zA-Z]+?(?=-|_|,|;)/)}`.toLowerCase();
-
-	return {
-		status: 301,
-		headers: { Location: `/${locale}` }
-	};
-};
+export const GET: RequestHandler = async ({ request }) => ({
+	status: 301,
+	headers: {
+		Location: `/${`${request.headers
+			.get('accept-language')
+			?.match(/[a-zA-Z]+?(?=-|_|,|;)/)}`.toLowerCase()}`
+	}
+});
