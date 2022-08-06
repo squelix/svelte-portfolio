3627421a2f4b3f9e092b953c510e0dddfc79a439
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-08-06 09:54:50 +0200

    Add all translations

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,10 +1,10 @@
+import { Routes, RoutesEnum } from '$lib/routing';
 import { LangEnum } from '$models/langs.enum';
 import lang from '$translations/lang.json';
 import i18n from '@sveltekit-i18n/base';
 import parser from '@sveltekit-i18n/parser-default';
 
 import type { Config } from '@sveltekit-i18n/parser-default';
-
 export const config: Config = {
 	translations: {
 		en: { lang },
@@ -37,14 +37,90 @@ export const config: Config = {
 		{
 			locale: LangEnum.fr_FR,
 			key: 'home',
-			routes: [''],
+			routes: ['', '/'],
 			loader: async () => (await import('./fr/home.json')).default
 		},
 		{
 			locale: LangEnum.en_GB,
 			key: 'home',
-			routes: [''],
+			routes: ['', '/'],
 			loader: async () => (await import('./en/home.json')).default
+		},
+		{
+			locale: LangEnum.fr_FR,
+			key: 'about',
+			routes: [
+				Routes[RoutesEnum.About],
+				Routes[RoutesEnum.AboutPersonalInfo],
+				Routes[RoutesEnum.AboutPersonalInfoBio],
+				Routes[RoutesEnum.AboutPersonalInfoBioLookingFor],
+				Routes[RoutesEnum.AboutPersonalInfoEducation],
+				Routes[RoutesEnum.AboutPersonalInfoEducationSchools],
+				Routes[RoutesEnum.AboutPersonalInfoEducationProjects]
+			],
+			loader: async () => (await import('./fr/about.json')).default
+		},
+		{
+			locale: LangEnum.en_GB,
+			key: 'about',
+			routes: [
+				Routes[RoutesEnum.About],
+				Routes[RoutesEnum.AboutPersonalInfo],
+				Routes[RoutesEnum.AboutPersonalInfoBio],
+				Routes[RoutesEnum.AboutPersonalInfoBioLookingFor],
+				Routes[RoutesEnum.AboutPersonalInfoEducation],
+				Routes[RoutesEnum.AboutPersonalInfoEducationSchools],
+				Routes[RoutesEnum.AboutPersonalInfoEducationProjects]
+			],
+			loader: async () => (await import('./en/about.json')).default
+		},
+		{
+			locale: LangEnum.fr_FR,
+			key: 'bio',
+			routes: [Routes[RoutesEnum.AboutPersonalInfoBio]],
+			loader: async () => (await import('./fr/bio.json')).default
+		},
+		{
+			locale: LangEnum.en_GB,
+			key: 'bio',
+			routes: [Routes[RoutesEnum.AboutPersonalInfoBio]],
+			loader: async () => (await import('./en/bio.json')).default
+		},
+		{
+			locale: LangEnum.fr_FR,
+			key: 'looking-for',
+			routes: [Routes[RoutesEnum.AboutPersonalInfoBioLookingFor]],
+			loader: async () => (await import('./fr/looking-for.json')).default
+		},
+		{
+			locale: LangEnum.en_GB,
+			key: 'looking-for',
+			routes: [Routes[RoutesEnum.AboutPersonalInfoBioLookingFor]],
+			loader: async () => (await import('./en/looking-for.json')).default
+		},
+		{
+			locale: LangEnum.fr_FR,
+			key: 'schools',
+			routes: [Routes[RoutesEnum.AboutPersonalInfoEducationSchools]],
+			loader: async () => (await import('./fr/schools.json')).default
+		},
+		{
+			locale: LangEnum.en_GB,
+			key: 'schools',
+			routes: [Routes[RoutesEnum.AboutPersonalInfoEducationSchools]],
+			loader: async () => (await import('./en/schools.json')).default
+		},
+		{
+			locale: LangEnum.fr_FR,
+			key: 'education-projects',
+			routes: [Routes[RoutesEnum.AboutPersonalInfoEducationProjects]],
+			loader: async () => (await import('./fr/education-projects.json')).default
+		},
+		{
+			locale: LangEnum.en_GB,
+			key: 'education-projects',
+			routes: [Routes[RoutesEnum.AboutPersonalInfoEducationSchools]],
+			loader: async () => (await import('./en/education-projects.json')).default
 		}
 	]
 };
