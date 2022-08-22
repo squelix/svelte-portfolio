d3aa41e74b076af7d9e17bc9272e5580ce849ba3
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-08-22 10:45:52 +0200

    Add technos icons recors

---
+++ src/lib/technos-icons.ts
@@ -0,0 +1,27 @@
+import angular from '$icons/angular.svg?raw';
+import css from '$icons/css.svg?raw';
+import flutter from '$icons/flutter.svg?raw';
+import gatsby from '$icons/gatsby.svg?raw';
+import html from '$icons/html.svg?raw';
+import javascript from '$icons/javascript.svg?raw';
+import react from '$icons/react.svg?raw';
+import scss from '$icons/scss.svg?raw';
+import svelte from '$icons/svelte.svg?raw';
+import typescript from '$icons/typescript.svg?raw';
+import vue from '$icons/vue.svg?raw';
+import xna from '$icons/xna.svg?raw';
+
+export const technosIcons: Record<string, string> = {
+	angular,
+	css,
+	flutter,
+	gatsby,
+	html,
+	javascript,
+	react,
+	scss,
+	svelte,
+	typescript,
+	vue,
+	xna
+};
