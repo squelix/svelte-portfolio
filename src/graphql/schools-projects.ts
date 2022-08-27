a2e4983d8095a17ea1ed9388f4e7ab8cd5fe952e
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-08-27 22:25:29 +0200

    Fix possibly appolo error

--- src/graphql/schools-projects.ts
+++ src/graphql/schools-projects.ts
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client/core';
+import { gql } from '@apollo/client/core/index.js';
 
 export const GET_SCHOOLS_PROJECTS_QUERY = gql`
 	query getSchoolsProjects($locale: I18NLocaleCode!) {
--- src/graphql/technos.ts
+++ src/graphql/technos.ts
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client/core';
+import { gql } from '@apollo/client/core/index.js';
 
 export const GET_TECHNOS_QUERY = gql`
 	query getTechnos {
