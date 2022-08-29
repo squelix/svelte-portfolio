5eb929fc71ee810bdd9fc5b87c54b2ab0a0db9db
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-08-29 14:08:26 +0200

    Add cache to graphql requests

--- src/stores/graphql.ts
+++ src/stores/graphql.ts
@@ -1,5 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call */
 import { graphqlUri } from '$stores/env';
-import { createClient } from '@urql/svelte';
+import { cacheExchange } from '@urql/exchange-graphcache';
+import { createClient, dedupExchange, fetchExchange } from '@urql/svelte';
 import { get, readable } from 'svelte/store';
 
-export const client = readable(createClient({ url: get(graphqlUri) }));
+export const client = readable(
+	createClient({
+		url: get(graphqlUri),
+		exchanges: [dedupExchange, cacheExchange({}), fetchExchange]
+	})
+);
