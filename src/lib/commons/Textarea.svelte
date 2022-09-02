0504dc4b9ab26c0b3748326825746b8ed650f26d
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-09-02 21:07:35 +0200

    Refactor some input duplicated CSS

--- src/lib/commons/Textarea.svelte
+++ src/lib/commons/Textarea.svelte
@@ -16,38 +16,13 @@
 </label>
 
 <style lang="scss">
-	@use 'lib/breakpoints' as br;
-	@use 'lib/font' as font;
+	@use 'lib/input' as input;
 
 	label {
-		@include font.fontWeight(450);
-
-		font-size: 1rem;
-		line-height: 1.3125;
-		color: var(--secondary-1);
+		@include input.label;
 	}
 
 	textarea {
-		display: block;
-		width: 100%;
-		background-color: var(--primary-3);
-		border: 1px solid var(--secondary-1);
-		border-radius: var(--border-radius);
-		box-shadow: 0 0 0 2px transparent;
-		transition: box-shadow var(--transition-duration) var(--transition-easing);
-		margin-top: 0.625rem;
-		padding: 0.625rem 0.9594rem;
-
-		&:disabled {
-			opacity: 0.3;
-			cursor: default;
-		}
-
-		&:not(:disabled) {
-			&:focus,
-			&:hover {
-				box-shadow: 0 0 0 2px rgb(96 123 150 / 0.3);
-			}
-		}
+		@include input.input;
 	}
 </style>
