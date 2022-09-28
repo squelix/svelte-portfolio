4d3192f9ac020ea82aa0216edf74965cfd437795
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-09-28 09:33:19 +0200

    Add socials to contact page with aria label

--- src/routes/[lang]/contact/+page.ts
+++ src/routes/[lang]/contact/+page.ts
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-non-null-assertion */
+import externalLinkSvg from '$icons/external-link.svg?raw';
 import mailSvg from '$icons/mail.svg?raw';
 import phoneSvg from '$icons/phone.svg?raw';
 import { contactPageNavItems } from '$lib/menu/nav';
 import { nav, setNavItem, setNavItems } from '$stores/nav';
-import { email, phone } from '$stores/profile';
+import { email, phone, socialNetworks } from '$stores/profile';
+import { t } from '$translations';
 import { get } from 'svelte/store';
 
 import type { PageLoad } from './$types';
@@ -18,14 +21,28 @@ export const load: PageLoad = () => {
 			id: 'contacts-email',
 			label: emailValue,
 			icon: mailSvg,
-			link: emailValue ? `mailto:${emailValue}` : undefined
+			link: emailValue ? `mailto:${emailValue}` : undefined,
+			ariaLabel: t.get('contact.aria.email')
 		},
 		{
 			id: 'contacts-phone',
 			label: phoneValue,
 			icon: phoneSvg,
-			link: phoneValue ? `tel:${phoneValue}` : undefined
+			link: phoneValue ? `tel:${phoneValue}` : undefined,
+			ariaLabel: t.get('contact.aria.phone')
 		}
 	]);
+
+	const socials = get(socialNetworks);
+	setNavItems(
+		'find-me-also-in',
+		socials?.map((item) => ({
+			id: item.attributes!.title,
+			label: item.attributes!.title,
+			link: item.attributes!.url,
+			icon: externalLinkSvg,
+			ariaLabel: t.get('contact.aria.social', { social: item.attributes!.title } as never)
+		})) ?? []
+	);
 	return {};
 };
