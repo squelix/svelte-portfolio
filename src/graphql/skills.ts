a9aa6c228e66c94b6a33763ede3ea9ec12bdf761
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-10-02 19:57:19 +0200

    Add skills graphql request

---
+++ src/graphql/skills.ts
@@ -0,0 +1,15 @@
+import { gql } from '@urql/svelte';
+
+export const GET_SKILLS_QUERY = gql`
+	query getSkills($locale: I18NLocaleCode) {
+		skills(locale: $locale, sort: "percentage:desc", pagination: { page: 1, pageSize: 50 }) {
+			data {
+				attributes {
+					name
+					percentage
+					slug
+				}
+			}
+		}
+	}
+`;
