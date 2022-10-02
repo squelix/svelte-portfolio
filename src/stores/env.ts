46c7d544a313e028a25ae6760759108634e61d35
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-10-02 18:34:52 +0200

    Disable graphql cache on dev mode

--- src/stores/env.ts
+++ src/stores/env.ts
@@ -1,15 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {
-	PUBLIC_GRAPHQL_URI,
-	PUBLIC_MAIL_ACCESS_TOKEN,
-	PUBLIC_MAIL_URI,
-	PUBLIC_RE_CAPTCHA_KEY,
-	PUBLIC_STRAPI_URI
+  PUBLIC_GRAPHQL_URI,
+  PUBLIC_MAIL_ACCESS_TOKEN,
+  PUBLIC_MAIL_URI,
+  PUBLIC_PLATFORM,
+  PUBLIC_RE_CAPTCHA_KEY,
+  PUBLIC_STRAPI_URI,
 } from '$env/static/public';
 import { derived, writable } from 'svelte/store';
 
 export const env = writable<Record<string, string>>({
 	graphqlUri: PUBLIC_GRAPHQL_URI,
+	platform: PUBLIC_PLATFORM,
 	strapiUri: PUBLIC_STRAPI_URI,
 	mailUri: PUBLIC_MAIL_URI,
 	mailAccessToken: PUBLIC_MAIL_ACCESS_TOKEN,
@@ -21,3 +23,4 @@ export const strapiUri = derived(env, ($env) => $env.strapiUri);
 export const mailUri = derived(env, ($env) => $env.mailUri);
 export const mailAccessToken = derived(env, ($env) => $env.mailAccessToken);
 export const reCaptchaKey = derived(env, ($env) => $env.reCaptchaKey);
+export const platform = derived(env, ($env) => $env.platform);
--- src/stores/graphql.ts
+++ src/stores/graphql.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any, @typescript-eslint/no-non-null-assertion */
-import { graphqlUri } from '$stores/env';
+import { graphqlUri, platform } from '$stores/env';
 import { cacheExchange } from '@urql/exchange-graphcache';
 import { createClient, dedupExchange, fetchExchange } from '@urql/svelte';
-import { get, readable } from 'svelte/store';
+import { derived, get } from 'svelte/store';
 
 import type {
 	Profile,
@@ -13,6 +13,8 @@ import type {
 	ProjectEntityResponse,
 	ProjectEntityResponseCollection,
 	SchoolProjectEntityResponseCollection,
+	Skill,
+	SkillEntity,
 	SocialNetwork,
 	SocialNetworkEntity,
 	SocialNetworkRelationResponseCollection,
@@ -22,52 +24,59 @@ import type {
 	TechnoEntityResponseCollection,
 	TechnoRelationResponseCollection
 } from '$models/graphql-generated';
-export const client = readable(
+
+export const client = derived(platform, ($platform) =>
 	createClient({
 		url: get(graphqlUri),
 		exchanges: [
 			dedupExchange,
-			cacheExchange({
-				keys: {
-					SocialNetwork: (item: any) => (item as SocialNetwork).slug,
-					SocialNetworkEntity: (item: any) => (item as SocialNetworkEntity).attributes!.slug,
-					SocialNetworkRelationResponseCollection: (item: any) =>
-						(item as SocialNetworkRelationResponseCollection).data
-							.map((item) => item.attributes!.slug)
-							.join('-'),
-					UploadFileEntityResponse: () => null,
-					UploadFile: () => null,
-					UploadFileEntity: () => null,
-					Profile: (item: any) => (item as Profile).slug,
-					ProfileEntity: (item: any) => (item as ProfileEntity).attributes!.slug,
-					ProfileEntityResponse: (item: any) =>
-						(item as ProfileEntityResponse).data!.attributes!.slug,
-					Techno: (item: any) => (item as Techno).slug,
-					TechnoEntity: (item: any) => (item as TechnoEntity).attributes!.slug,
-					TechnoEntityResponse: (item: any) =>
-						(item as TechnoEntityResponse).data!.attributes!.slug,
-					TechnoEntityResponseCollection: (item: any) =>
-						(item as TechnoEntityResponseCollection).data
-							.map((item) => item.attributes!.slug)
-							.join('-'),
-					TechnoRelationResponseCollection: (item: any) =>
-						(item as TechnoRelationResponseCollection).data
-							.map((item) => item.attributes!.slug)
-							.join('-'),
-					Project: (item: any) => (item as Project).slug,
-					ProjectEntity: (item: any) => (item as ProjectEntity).attributes!.slug,
-					ProjectEntityResponse: (item: any) =>
-						(item as ProjectEntityResponse).data!.attributes!.slug,
-					ProjectEntityResponseCollection: (item: any) =>
-						(item as ProjectEntityResponseCollection).data
-							.map((item) => item.attributes!.slug)
-							.join('-'),
-					SchoolProjectEntityResponseCollection: (item: any) =>
-						(item as SchoolProjectEntityResponseCollection).data
-							.map((item) => item.attributes!.slug)
-							.join('-')
-				}
-			}),
+			...($platform === 'dev'
+				? []
+				: [
+						cacheExchange({
+							keys: {
+								Skill: (item: any) => (item as Skill).slug!,
+								SkillEntity: (item: any) => (item as SkillEntity).attributes!.slug!,
+								SocialNetwork: (item: any) => (item as SocialNetwork).slug,
+								SocialNetworkEntity: (item: any) => (item as SocialNetworkEntity).attributes!.slug,
+								SocialNetworkRelationResponseCollection: (item: any) =>
+									(item as SocialNetworkRelationResponseCollection).data
+										.map((item) => item.attributes!.slug)
+										.join('-'),
+								UploadFileEntityResponse: () => null,
+								UploadFile: () => null,
+								UploadFileEntity: () => null,
+								Profile: (item: any) => (item as Profile).slug,
+								ProfileEntity: (item: any) => (item as ProfileEntity).attributes!.slug,
+								ProfileEntityResponse: (item: any) =>
+									(item as ProfileEntityResponse).data!.attributes!.slug,
+								Techno: (item: any) => (item as Techno).slug,
+								TechnoEntity: (item: any) => (item as TechnoEntity).attributes!.slug,
+								TechnoEntityResponse: (item: any) =>
+									(item as TechnoEntityResponse).data!.attributes!.slug,
+								TechnoEntityResponseCollection: (item: any) =>
+									(item as TechnoEntityResponseCollection).data
+										.map((item) => item.attributes!.slug)
+										.join('-'),
+								TechnoRelationResponseCollection: (item: any) =>
+									(item as TechnoRelationResponseCollection).data
+										.map((item) => item.attributes!.slug)
+										.join('-'),
+								Project: (item: any) => (item as Project).slug,
+								ProjectEntity: (item: any) => (item as ProjectEntity).attributes!.slug,
+								ProjectEntityResponse: (item: any) =>
+									(item as ProjectEntityResponse).data!.attributes!.slug,
+								ProjectEntityResponseCollection: (item: any) =>
+									(item as ProjectEntityResponseCollection).data
+										.map((item) => item.attributes!.slug)
+										.join('-'),
+								SchoolProjectEntityResponseCollection: (item: any) =>
+									(item as SchoolProjectEntityResponseCollection).data
+										.map((item) => item.attributes!.slug)
+										.join('-')
+							}
+						})
+				  ]),
 			fetchExchange
 		]
 	})
