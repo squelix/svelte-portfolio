f1b132541aa68e48c5c7a64e27b9140005cf4f65
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-10-02 18:35:10 +0200

    Add default nav select on page level

---
+++ src/routes/[lang]/about/personal-info/+layout.ts
@@ -0,0 +1,9 @@
+import { AboutPageNavItemEnum } from '$lib/menu/about-page-nav-item.enum';
+import { setNavItem } from '$stores/nav';
+
+import type { LayoutLoad } from './$types';
+
+export const load: LayoutLoad = () => {
+	setNavItem(AboutPageNavItemEnum.PersonalInfo);
+	return {};
+};
