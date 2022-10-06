c1864fdd1da5dea465f860eea0aa1d0d95fcb4de
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-10-06 18:48:16 +0200

    Add languages store

---
+++ src/stores/languages.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unused-vars */
+import { generateUnicodeBar } from '$lib/unicode-bar';
+import { derived, writable } from 'svelte/store';
+
+import type { GetLanguagesQuery } from '$models/graphql-generated';
+
+export const languages = writable<GetLanguagesQuery | undefined>();
+
+export const languagesList = derived(languages, ($languages) =>
+	($languages?.languages?.data ?? []).map((language) => language.attributes)
+);
+
+export const languagesListBars = derived(
+	languagesList,
+	($languagesList) =>
+		$languagesList
+			.filter((language) => !!language)
+			.map((language) => {
+				if (!language) {
+					return undefined;
+				}
+				const bar = generateUnicodeBar(language.percentage);
+				if (!bar) {
+					return undefined;
+				}
+				return `${language.name} : ${bar} (${language.percentage}%)`;
+			})
+			.filter((language) => !!language) as string[]
+);
