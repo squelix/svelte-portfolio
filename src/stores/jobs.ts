7890016f5c3af905d9731e2813c56650c320d4fb
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-10-11 13:49:26 +0200

    Add jobs stores

---
+++ src/stores/jobs.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unused-vars */
+import { derived, writable } from 'svelte/store';
+
+import type { GetJobsQuery, Job } from '$models/graphql-generated';
+import type { Readable } from 'svelte/store';
+
+export const jobs = writable<GetJobsQuery | undefined>();
+
+export const jobsList: Readable<Omit<Job, '__typename'>[]> = derived(
+	jobs,
+	($jobs) =>
+		($jobs?.jobs?.data ?? [])
+			.map((job) => job.attributes)
+			.map((job) => {
+				if (job?.__typename) {
+					const { __typename, ...j } = job;
+					return j;
+				}
+				return job;
+			}) as Omit<Job, '__typename'>[]
+);
