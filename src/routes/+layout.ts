f3e53895144dfc180847ef69827459274724e3a5
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-10-11 13:47:21 +0200

    Fix month name in dayjs

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,6 +3,7 @@ import dayjs from 'dayjs';
 import 'dayjs/locale/en.js';
 import 'dayjs/locale/fr.js';
 import localeData from 'dayjs/plugin/localeData.js';
+import updateLocale from 'dayjs/plugin/updateLocale.js';
 
 import type { LayoutLoad } from './$types';
 
@@ -10,6 +11,39 @@ export const prerender = 'auto';
 
 export const load: LayoutLoad = ({ data }) => {
 	dayjs.extend(localeData);
+	dayjs.extend(updateLocale);
 	dayjs.locale(data.lang);
+	dayjs.updateLocale('en', {
+		months: [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December'
+		]
+	});
+	dayjs.updateLocale('fr', {
+		months: [
+			'Janvier',
+			'Février',
+			'Mars',
+			'Avril',
+			'Mai',
+			'Juin',
+			'Juillet',
+			'Août',
+			'Septembre',
+			'Octobre',
+			'Novembre',
+			'Décembre'
+		]
+	});
 	return data;
 };
