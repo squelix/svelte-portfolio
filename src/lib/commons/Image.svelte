6106014410c7e5ca4db33bd3e8a1c07686054702
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-10-14 16:37:28 +0200

    Update Image component to set style

--- src/lib/commons/Image.svelte
+++ src/lib/commons/Image.svelte
@@ -11,6 +11,7 @@
 </script>
 
 <img
+	style={$$props.style}
 	class={$$props.class}
 	src={images.image1x}
 	srcset={`${images.image1x} 1x, ${images.image2x} 2x, ${images.image3x} 3x, ${images.image4x} 4x`}
