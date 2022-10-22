cf394bf9d7ccc18220e5d2e6dbbeb0acff7528ce
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-10-22 18:06:32 +0200

    Fix icons

--- src/lib/technos-icons.ts
+++ src/lib/technos-icons.ts
@@ -1,16 +1,18 @@
+import universal from '$icons/angular-universal.svg?raw';
 import angular from '$icons/angular.svg?raw';
+import xna from '$icons/c-xna.svg?raw';
 import css from '$icons/css.svg?raw';
 import flutter from '$icons/flutter.svg?raw';
 import gatsby from '$icons/gatsby.svg?raw';
 import html from '$icons/html.svg?raw';
 import ionic from '$icons/ionic.svg?raw';
 import javascript from '$icons/javascript.svg?raw';
+import ngxs from '$icons/ng-xs.svg?raw';
 import react from '$icons/react.svg?raw';
 import scss from '$icons/scss.svg?raw';
 import svelte from '$icons/svelte.svg?raw';
 import typescript from '$icons/typescript.svg?raw';
 import vue from '$icons/vue.svg?raw';
-import xna from '$icons/xna.svg?raw';
 
 export const technosIcons: Record<string, string> = {
 	angular,
@@ -23,7 +25,9 @@ export const technosIcons: Record<string, string> = {
 	scss,
 	svelte,
 	typescript,
+	ionic,
 	vue,
-	xna,
-	ionic
+	['angular-universal']: universal,
+	['c-xna']: xna,
+	['ng-xs']: ngxs
 };
