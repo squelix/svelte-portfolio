21ecd3774f317bffbaee52eac458ff0b45e74b0c
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-11-08 09:52:02 +0100

    Refactor validatrs email

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -1,6 +1,5 @@
 /* eslint-disable no-useless-escape */
-export const validateEmail = (email: string) => {
-	const re =
-		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(String(email).toLowerCase());
-};
+export const validateEmail = (email: string) =>
+	/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+		String(email).toLowerCase()
+	);
