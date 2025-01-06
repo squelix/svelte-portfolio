d82e1b7f1b16adea8d7f513fda3a1ce074401ece
Mickaël Depardon <mickael.depardon.w@gmail.com>
2025-01-06 16:14:13 +0100

    fix: recaptcha

--- src/routes/[lang]/api/mail/+server.ts
+++ src/routes/[lang]/api/mail/+server.ts
@@ -5,47 +5,45 @@ import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ request, fetch }) => {
-	try {
-		const { token, subject, text, html, email } = await request.json();
-		const accessToken = request.headers.get('x-access-token');
-		const myAccessToken = env.MAIL_ACCESS_TOKEN;
+	const { token, subject, text, html, email } = await request.json();
+	const accessToken = request.headers.get('x-access-token');
+	const myAccessToken = env.MAIL_ACCESS_TOKEN;
 
-		if (!token || !subject || !text || !html || !email) {
-			return error(400, 'Bad Request');
-		}
-
-		if (!accessToken || accessToken !== myAccessToken) {
-			return error(401, 'Unauthorized');
-		}
-
-		const params = new URLSearchParams({
-			secret: env.RE_CAPTCHA_PRIVATE_KEY,
-			response: token
-		});
-		const { success } = await fetch(
-			`https://www.google.com/recaptcha/api/siteverify?${params.toString()}`,
-			{
-				method: 'POST'
-			}
-		).then((response) => response.json());
-
-		if (!success) {
-			return error(422, 'Captcha not good');
-		} else {
-			sgMail.setApiKey(env.SENDGRID_API_KEY);
-
-			await sgMail.send({
-				to: env.SENDGRID_SENDER,
-				from: env.SENDGRID_SENDER,
-				replyTo: email,
-				subject,
-				text,
-				html
-			});
-			return new Response(undefined, { status: 204 });
-		}
-	} catch (err) {
-		console.error(err);
-		return error(500, err as string | undefined);
+	if (!token || !subject || !text || !html || !email) {
+		return error(400, 'Bad Request');
 	}
+
+	if (!accessToken || accessToken !== myAccessToken) {
+		return error(401, 'Unauthorized');
+	}
+
+	const params = new URLSearchParams({
+		secret: env.RE_CAPTCHA_PRIVATE_KEY,
+		response: token
+	});
+	const success = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+		method: 'POST',
+		body: params.toString(),
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+		}
+	})
+		.then((response) => response.json())
+		.then(({ success }) => success);
+
+	if (!success) {
+		return error(422, 'Captcha not good');
+	}
+
+	sgMail.setApiKey(env.SENDGRID_API_KEY);
+
+	await sgMail.send({
+		to: env.SENDGRID_SENDER,
+		from: env.SENDGRID_SENDER,
+		replyTo: email,
+		subject,
+		text,
+		html
+	});
+	return new Response(undefined, { status: 204 });
 };
