a79039f8d99d0f33efa1d602b1bd7fbd8be8ff47
Mickaël Depardon <mickael.depardon.w@gmail.com>
2025-01-06 15:56:59 +0100

    fix: use node runtime everywhere

--- src/routes/[lang]/+layout.ts
+++ src/routes/[lang]/+layout.ts
@@ -2,12 +2,7 @@ import { loadTranslations } from '$translations';
 import 'dayjs/locale/en.js';
 import 'dayjs/locale/fr.js';
 
-import type { Config } from '@sveltejs/adapter-vercel';
 import type { LayoutLoad } from './$types';
-
-export const config: Config = {
-	runtime: 'edge'
-};
 
 export const load: LayoutLoad = async ({ data: { route, lang, profile } }) => {
 	await loadTranslations(lang, route);
